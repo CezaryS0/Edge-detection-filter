dfe4f3cdce7e0baca3c8e235cca6e4b4a4d28211
Cezary Szumerowski <czarek.szumerowski@gmail.com>
2021-11-13 11:18:58 +0100

    Update

--- Prewitt/Prewitt/CSharpPrewitt.cs
+++ Prewitt/Prewitt/CSharpPrewitt.cs
@@ -48,22 +48,13 @@ namespace Prewitt
                            sourceBitmap.Width, sourceBitmap.Height),
                                              ImageLockMode.ReadOnly,
                                         PixelFormat.Format32bppArgb);
+            byte[] pixelBuffer = new byte[sourceData.Stride *sourceData.Height];
 
+            byte[] resultBuffer = new byte[sourceData.Stride *sourceData.Height];
 
-            byte[] pixelBuffer = new byte[sourceData.Stride *
-                                          sourceData.Height];
-
-
-            byte[] resultBuffer = new byte[sourceData.Stride *
-                                           sourceData.Height];
-
-
-            Marshal.Copy(sourceData.Scan0, pixelBuffer, 0,
-                                       pixelBuffer.Length);
-
+            Marshal.Copy(sourceData.Scan0, pixelBuffer, 0, pixelBuffer.Length);
 
             sourceBitmap.UnlockBits(sourceData);
-
 
             if (grayscale == true)
             {
@@ -114,72 +105,21 @@ namespace Prewitt
                     blueX = greenX = redX = 0;
                     blueY = greenY = redY = 0;
                     blueTotal = greenTotal = redTotal = 0.0;
-                    byteOffset = offsetY *
-                                 sourceData.Stride +
-                                 offsetX * 4;
+                    byteOffset = offsetY *sourceData.Stride +offsetX * 4;
 
-
-                    for (int filterY = -filterOffset;
-                        filterY <= filterOffset; filterY++)
+                    for (int filterY = -filterOffset;filterY <= filterOffset; filterY++)
                     {
-                        for (int filterX = -filterOffset;
-                            filterX <= filterOffset; filterX++)
+                        for (int filterX = -filterOffset;filterX <= filterOffset; filterX++)
                         {
-                            calcOffset = byteOffset +
-                                         (filterX * 4) +
-                                         (filterY * sourceData.Stride);
-
-
-                            blueX += (double)
-                                      (pixelBuffer[calcOffset]) *
-                                      xFilterMatrix[filterY +
-                                                    filterOffset,
-                                                    filterX +
-                                                    filterOffset];
-
-
-                            greenX += (double)
-                                  (pixelBuffer[calcOffset + 1]) *
-                                      xFilterMatrix[filterY +
-                                                    filterOffset,
-                                                    filterX +
-                                                    filterOffset];
-
-
-                            redX += (double)
-                                  (pixelBuffer[calcOffset + 2]) *
-                                      xFilterMatrix[filterY +
-                                                    filterOffset,
-                                                    filterX +
-                                                    filterOffset];
-
-
-                            blueY += (double)
-                                      (pixelBuffer[calcOffset]) *
-                                      yFilterMatrix[filterY +
-                                                    filterOffset,
-                                                    filterX +
-                                                    filterOffset];
-
-
-                            greenY += (double)
-                                  (pixelBuffer[calcOffset + 1]) *
-                                      yFilterMatrix[filterY +
-                                                    filterOffset,
-                                                    filterX +
-                                                    filterOffset];
-
-
-                            redY += (double)
-                                  (pixelBuffer[calcOffset + 2]) *
-                                      yFilterMatrix[filterY +
-                                                    filterOffset,
-                                                    filterX +
-                                                    filterOffset];
+                            calcOffset = byteOffset +(filterX * 4) +(filterY * sourceData.Stride);
+                            blueX += (double)(pixelBuffer[calcOffset]) *xFilterMatrix[filterY +filterOffset,filterX +filterOffset];
+                            greenX += (double)(pixelBuffer[calcOffset + 1]) *xFilterMatrix[filterY +filterOffset,filterX +filterOffset];
+                            redX += (double)(pixelBuffer[calcOffset + 2]) *xFilterMatrix[filterY +filterOffset,filterX +filterOffset];
+                            blueY += (double)(pixelBuffer[calcOffset]) *yFilterMatrix[filterY +filterOffset,filterX +filterOffset];
+                            greenY += (double)(pixelBuffer[calcOffset + 1]) *yFilterMatrix[filterY +filterOffset,filterX +filterOffset];
+                            redY += (double)(pixelBuffer[calcOffset + 2]) * yFilterMatrix[filterY +filterOffset,filterX + filterOffset];
                         }
                     }
-
-
                     blueTotal = Math.Sqrt((blueX * blueX) +(blueY * blueY));
                     greenTotal = Math.Sqrt((greenX * greenX) +(greenY * greenY));
                     redTotal = Math.Sqrt((redX * redX) +(redY * redY));
@@ -191,16 +131,15 @@ namespace Prewitt
 
 
                     if (greenTotal > 255)
-                    { greenTotal = 255; }
+                        greenTotal = 255;
                     else if (greenTotal < 0)
-                    { greenTotal = 0; }
+                        greenTotal = 0;
 
 
                     if (redTotal > 255)
-                    { redTotal = 255; }
+                        redTotal = 255;
                     else if (redTotal < 0)
-                    { redTotal = 0; }
-
+                        redTotal = 0; 
 
                     resultBuffer[byteOffset] = (byte)(blueTotal);
                     resultBuffer[byteOffset + 1] = (byte)(greenTotal);
@@ -210,21 +149,14 @@ namespace Prewitt
             }
 
 
-            Bitmap resultBitmap = new Bitmap(sourceBitmap.Width,
-                                             sourceBitmap.Height);
+            Bitmap resultBitmap = new Bitmap(sourceBitmap.Width,sourceBitmap.Height);
 
 
-            BitmapData resultData =
-                       resultBitmap.LockBits(new Rectangle(0, 0,
-                       resultBitmap.Width, resultBitmap.Height),
-                                        ImageLockMode.WriteOnly,
-                                    PixelFormat.Format32bppArgb);
+            BitmapData resultData =resultBitmap.LockBits(new Rectangle(0, 0,resultBitmap.Width, resultBitmap.Height),ImageLockMode.WriteOnly,PixelFormat.Format32bppArgb);
 
 
-            Marshal.Copy(resultBuffer, 0, resultData.Scan0,
-                                       resultBuffer.Length);
+            Marshal.Copy(resultBuffer, 0, resultData.Scan0,resultBuffer.Length);
             resultBitmap.UnlockBits(resultData);
-
 
             return resultBitmap;
         }
