5779cca6fa3f27a95ffe5bded605bdf9af5212e1
Cezary Szumerowski <czarek.szumerowski@gmail.com>
2022-01-14 09:52:10 +0100

    Update

--- Prewitt/Prewitt/ASMPrewitt.cs
+++ Prewitt/Prewitt/ASMPrewitt.cs
@@ -3,7 +3,7 @@ namespace Prewitt
 {
     public class ASMPrewitt
     {
-        [DllImport(@"C:\Users\czare\source\repos\Edge-detection-filter\Prewitt\x64\Debug\PrewittASM.dll")]
+        [DllImport(@"PrewittASM.dll")]
         static extern void ASMPrewittDLL(byte[] pixelBuffer, double[] RGB, int byteOffset, int stride);
         public static void ASMFilter(ref byte[] pixelbuffer, ref double[] RGB, int byteOffset, int stride)
         {
